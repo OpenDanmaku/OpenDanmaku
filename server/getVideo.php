3ba4263a199bb6563f0805ecbc0c9b88d2647482
Schezuk <schezuk@163.com>
2014-12-06 03:30:58 +0800

    ver 0.0.1
    初稿,主要更新服务端

--- server/getVideo.php
+++ server/getVideo.php
@@ -17,7 +17,7 @@
 		// 关闭数据库,退出
 		$mysql->closeDb();
 		exit;
-	}
+		}
 	if(strtolower($_REQUEST['action'])= "view"){//按播放量倒序获取最近7天视频
 		$sql = "SELECT LOWER(HEX(`btih`)), `uid`, `time`, `view`, `reply` FROM `video` ";
 		$sql.= "WHERE ~time~ < " .time()-7*24*60*60 ." ORDER BY `view` DESC;";
@@ -29,8 +29,8 @@
 		// 关闭数据库,退出
 		$mysql->closeDb();
 		exit;
-	}
-	if(strtolower($_REQUEST['action'])="reply"){//按播放量倒序获取最近7天视频
+		}
+	if(strtolower($_REQUEST['action'])="reply"){//按弹幕量倒序获取最近7天视频
 		$sql = "SELECT LOWER(HEX(`btih`)), `uid`, `time`, `view`, `reply` FROM `video` ";
 		$sql.= "WHERE ~time~ < " .time()-7*24*60*60 ." ORDER BY `reply` DESC;";
 		//执行SQL语句,出错则报错,否则返回值
@@ -41,30 +41,30 @@
 		// 关闭数据库,退出
 		$mysql->closeDb();
 		exit;
+		}
+	}else{	//都不是则视为查询btih,形式上建议用参数action=find,虽然不会去检验
+		//查询BTIH
+		if (!isset($_REQUEST['btih'])) die("Error: BTIH Is Not Specified");
+		$btih=(string)$_REQUEST['btih'];
+	
+		//检验BTIH有效性并小写化,"magnet:?xt=urn:btih:"长度为20,btih长度为40
+		if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0)	//如果是完整磁链
+			$btih=substr($btih,20,40);										//截取btih
+		if(strlen($btih)!==40 or !ctype_xdigit($btih)))//防注入
+			die("Error: Link Not Valid.");
+		$btih= strtolower($btih);
+	
+		//设置SQL语句,按BTIH筛选,取所有信息
+		$sql = "SELECT LOWER(HEX(`btih`)), `uid`, `time`, `view`, `reply` FROM `video`";
+		$sql.= "WHERE `btih` = x'" . $btih . "';";
+		
+		//执行SQL语句,出错则报错,否则返回值
+		$video = $mysql->getLine($sql);
+		if ($mysql->errno() != 0) die("Error:" . $mysql->errmsg());//出错
+		if ($mysql->affectedRows()!==1) die("Error: BTIH Does Not Exists");//返回空
+		echo json_encode($video);//返回json对象
+		
+		// 关闭数据库
+		$mysql->closeDb();
 	}
-	//都不是则视为查询btih,形式上建议用参数action=find,虽然不会去检验
-	}		
-	//查询BTIH
-	if (!isset($_REQUEST['btih'])) die("Error: BTIH Is Not Specified");
-	$btih=(string)$_REQUEST['btih'];
-
-	//检验BTIH有效性并小写化,"magnet:?xt=urn:btih:"长度为20,btih长度为40
-	if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0)
-		$btih=substr($btih,20,40);
-	if(strlen($btih)!==40 or !ctype_xdigit($btih)))//防注入
-		die("Link Not Valid.");
-	$btih= strtolower($btih);
-
-	//设置SQL语句,按BTIH筛选,取所有信息
-	$sql = "SELECT LOWER(HEX(`btih`)), `uid`, `time`, `view`, `reply` FROM `video`";
-	$sql.= "WHERE `btih` = x'" . $btih . "';";
-	
-	//执行SQL语句,出错则报错,否则返回值
-	$video = $mysql->getLine($sql);
-	if ($mysql->errno() != 0) die("Error:" . $mysql->errmsg());//出错
-	if ($mysql->affectedRows()!==1) die("Error: BTIH Does Not Exists");//返回空
-	echo json_encode($video);//返回json对象
-	
-	// 关闭数据库
-	$mysql->closeDb();
 ?>

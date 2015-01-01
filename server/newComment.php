e7a6521a298352731e35c716ca28378ea8b0aa23
Schezuk <schezuk@163.com>
2015-01-01 12:44:03 +0800

    Update newComment.php

--- server/newComment.php
+++ server/newComment.php
@@ -24,15 +24,16 @@ $the_time_now=time();
 $new_comment=trim((string)$_REQUEST['comment']);
 //读取参数btih,并字符串化,小写化
 $btih=trim(strtolower(strval($_REQUEST['btih'])));//读取参数btih
-//如果是完整磁链,截取btih,"magnet:?xt=urn:btih:"长度为20,btih长度为40
-if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0) $btih=substr($btih,20,40);
+//如果是完整磁链,截取btih,,btih长度为40
+$pos=strpos($btih,"btih:");
+if($pos !== false) $btih=substr($btih,$pos+5,40);
 //检验btih有效性,即使btih仅由0-9组成也没关系,因为代码中不存在hex与unhex
 if(strlen($btih)!==40 or !ctype_xdigit($btih)) die(json_err('btih_incorrect',-1,'Error: Link Not Correct'));
 
 //查询视频是否已经存在,如btih不存在,退出
 $result=NULL;
-$count=safe_query("SELECT `reply`, `c_index` LENGTH(`comment`) FROM `video` WHERE `btih` = x?;", &$result, array('s',$btih));
-//???????作为string处理是否可行?待验证
+$count=safe_query("SELECT `reply`, `c_index` LENGTH(`comment`) FROM `video` WHERE `btih` = UNHEX(?);",
+		&$result, array('s',$btih));//http://stackoverflow.com/questions/1747894/
 if($count!=1) die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//返回空
 
 //编辑弹幕{"c":"sec.000,color=FFFFFF,type(1),size(25),uid,timestamp","m":"text","cid":1},
@@ -41,7 +42,7 @@ $new_comment = json_decode($new_comment);		//json->array
 		$array_comment[4]=strval($uid)			//strval是因为要合并字符串
 		$array_comment[5]=strval($the_time_now);	//strval是因为要合并字符串
 	$new_comment['c']=implode(",",$array_comment);
-	$new_comment['cid']=intval($result['reply']);	//reply为弹幕总数,即最大下标+1
+	$new_comment['cid']=intval($result[0]['reply']);	//reply为弹幕总数,即最大下标+1
 $new_comment = json_encode($new_comment);		//array->json
 $new_comment.= ',';					//结尾添加逗号
 
@@ -49,12 +50,12 @@ $new_comment.= ',';					//结尾添加逗号
 $c_index = json_decode($c_index);	//json->array
 //检验错误
 $c_count = count($c_index);
-if($result['reply']!=$c_count)
+if($result[0]['reply']!=$c_count)
 	die(json_err('reply_countnotmatch',-1,'Error: Fatal Error! Counting Does not Match. Please Report to Admin!'));
-if($result['LEN(`comment`)']!=$c_index[$c_count-1]['size']){
+if($result[0]['LEN(`comment`)']!=$c_index[$c_count-1]['size']){
 	die(json_err('reply_lengthnotmatch',-1,'Error: Fatal Error! Length Does not Match. Please Report to Admin!'));
 //编辑索引[uid,time,size]
-$c_index[]=array($uid,$the_time_now,$result['LENGTH(`comment`)']+strlen($new_comment));
+$c_index[]=array($uid,$the_time_now,$result[0]['LENGTH(`comment`)']+strlen($new_comment));
 $c_index = json_encode($c_index);	//array->json	
 ++$c_count;
 

40ae9d3a71fad7bc6da9f3f29169dafcbb1692c8
Schezuk <schezuk@163.com>
2015-01-01 14:19:59 +0800

    Update newDislike.php

--- server/newDislike.php
+++ server/newDislike.php
@@ -1,59 +1,43 @@
-﻿<?php
-	header("Access-Control-Allow-Origin: *");//无限制
-	
-	//硬直与禁言设定
-	$const_ScoreNewDislike = -20;//减20分
-	$const_DelayNewDislike = 30;//30秒硬直
-	$const_DelayRate = 60*60/5;//扣光积分后,每个人的10点仇恨后折合双方禁言4小时
-	//$_GET和$_REQUEST已经urldecode()了！
+<?php
+require('libMysqli.php');
+header("Access-Control-Allow-Origin: *");//无限制
 
-	//打开MySQL。打开KVDB
-	$mysql = new SaeMysql();
-	$kv = new SaeKV();
-	if(!$kv->init()) die("Error:" . $kv->errno());//出错
+//硬直与禁言设定
+$const_ScoreNewDislike = -20;//减20分
+$const_DelayNewDislike = 30;//30秒硬直
+$const_DelayRate = 60*60/5;//扣光积分后,每个人的10点仇恨后折合双方禁言4小时
 
-	//如果有旧Cookie
-	if(isset($_COOKIE['uid'])){//获取Cookie对应用户数据,如果key不符合,退出
-		$sql = "SELECT * FROM `user` WHERE `uid` = ";
-		$sql.= (string)intval($_COOKIE['uid']) . ";";//防注入
-		$userC= $mysql->getLine($sql);
-		if($mysql->errno()!= 0)
-			die("Error:" . $mysql->errmsg());//SQL出错
-		if($mysql->affectedRows()!=1)
-			die("Error: Cookie Not Exists"); //uid不存在
-		if($userC['key']!=$_COOKIE['key'])
-			die("Error: Invalid Cookie");    //key不符合,!=代表作为数字比较
-		if($userC['status']==0)
-			die("Error: Deleted Cookie");    //status不活跃,==代表作为数字比较
-		if($userC['time']>time())
-			die("Error: Not Yet ");          //time还在硬直中
-	} else die("No Cookie");
-	
-//读取参数BTIH
-	//检验BTIH有效性并小写化,"magnet:?xt=urn:btih:"长度为20,btih长度为40
-	//即使btih仅由0-9组成也没关系,因为代码中不存在hex与unhex
-	$btih=(string)$_REQUEST['btih'];//字符串
-	if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0)	//如果是完整磁链
-		$btih=substr($btih,20,40);										//截取btih
-	if(strlen($btih)!==40 or !ctype_xdigit($btih))//防注入
-		die("Error: Link Not Valid.");
-	$btih= strtolower($btih);
-	//cid
-	$cid=(int)$_REQUEST['cid'];//数字
-	$uid=intval($_COOKIE['uid']);
-//查询btih是否还不存在
-	//查询视频是否已经存在,如BTIH不存在,退出
-	$sql = "SELECT * FROM `video` WHERE `btih` = x'" . $btih . "';";
-	$check = $mysql->getLine($sql);
-	if($mysql->errno()!= 0)
-		die("Error:" . $mysql->errmsg());//出错
-	if($mysql->affectedRows()!=1)
-		die("Error: Video Not Yet Exists, Do You Want to Create It?"); //返回空
+//cid
+$cid=intval(trim($_REQUEST['cid']));//数字
+//$_GET和$_REQUEST已经urldecode()了！
 
-//KV读取
-	if(!$dislike = $kv->get($btih . ",d" )) die("Error:" . $kv->errno());//赋值运算表达式的值也就是所赋的值
-	if(!$d_index = $kv->get($btih . ",di")) die("Error:" . $kv->errno());//赋值运算表达式的值也就是所赋的值
+//如果没有Cookie
+if(!isset($_COOKIE['uid'])) die(json_err('cookie_empty',-1,'Error: No Cookie Submitted'));//返回空
+$uid=intval($_COOKIE['uid']);
+
+//获取Cookie对应用户数据,如果key不符合,退出
+$result=NULL;
+$count=safe_query('SELECT * FROM `user` WHERE `uid` = ?;', &$result, array('i',$uid));
+if($count!=1) die(json_err('cookie_invalid',-1,'Error: Invalid Cookie'));//返回空
+//!= == >= 代表作为数字比较
+if($result[0]['key']!=$_COOKIE['key']) die(json_err('cookie_wrongkey',-1,'Error: Cookie with Wrong Key'));//key不符合
+if($result[0]['status']==0) die(json_err('cookie_deleted',-1,'Error: Deleted Cookie'));//status禁用
+if($result[0]['time']>=0) die(json_err('cookie_inactive',-1,'Error: Not Yet Active'));//time还在硬直中
+
+//读取参数btih,并字符串化,小写化
+$btih=trim(strtolower(strval($_REQUEST['btih'])));//读取参数btih
+//如果是完整磁链,截取btih,btih长度为40
+$pos=strpos($btih,"btih:");//len('btih:')===5
+$btih=($pos===FALSE)?substr($btih,$pos+5,40):substr($btih,0,40);//注意$pos会自动转换,而$pos=0和$pos=FALSE截取时有区别
+//检验btih长度(应该<=40)与有效性,即使btih仅由0-9组成也没关系,参见http://www.cnblogs.com/mincyw/archive/2011/02/10/1950733.html
+if(strlen($btih)!==40 or !ctype_xdigit($btih)) die(json_err('btih_incorrect',-1,'Error: Link Not Correct'));
 	
+//查询视频是否已经存在,如btih不存在,退出
+$result=NULL;
+$count=safe_query("SELECT `dislike` FROM `video` WHERE `btih` = UNHEX(?);",//d_index出错不会有严重影响，只要更新就好了
+		&$result, array('s',$btih));//http://stackoverflow.com/questions/1747894/
+if($count!=1) die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//返回空
+
 //编辑键值
 	$d_index = json_decode($d_index);//json->array
 	if(!isset($dislike[(string)$cid])) 

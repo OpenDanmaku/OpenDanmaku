ad2b74a5408522cd0bac36350aecacc97d762db4
Schezuk <schezuk@163.com>
2015-01-03 11:09:51 +0800

    test

--- server/newVideo.php
+++ server/newVideo.php
@@ -1,93 +1,53 @@
-﻿<?php
-	header("Access-Control-Allow-Origin: *");//无限制
+<?php
+require('libMysqli.php');
+header("Access-Control-Allow-Origin: *");//无限制
 	
-	//硬直与禁言设定
-	$const_ScoreNewVideo = 10;//加10分
-	$const_DelayNewVideo = 60;//60秒硬直
-	//$_GET和$_REQUEST已经urldecode()了！
+//硬直与禁言设定
+$const_ScoreNewVideo = 10;//加10分
+$const_DelayNewVideo = 60;//60秒硬直
 
-	//打开MySQL。打开KVDB
-	$mysql = new SaeMysql();
-	$kv = new SaeKV();
-	if(!$kv->init()) die("Error:" . $kv->errno());//出错
+//如果没有Cookie
+if(!isset($_COOKIE['uid'])) die(json_err('cookie_empty',-1,'Error: No Cookie Submitted'));//返回空
+$uid=intval($_COOKIE['uid']);
+//获取Cookie对应用户数据,如果key不符合,退出
+$result=NULL;
+$count=safe_query('SELECT * FROM `user` WHERE `uid` = ?;', &$result, array('i',$uid));
+if($count!=1) die(json_err('cookie_invalid',-1,'Error: Invalid Cookie'));//返回空
+//!= == >= 代表作为数字比较
+if($result[0]['key']!=$_COOKIE['key']) die(json_err('cookie_wrongkey',-1,'Error: Cookie with Wrong Key'));//key不符合
+if($result[0]['status']==0) die(json_err('cookie_deleted',-1,'Error: Deleted Cookie'));//status禁用
+if($result[0]['time']>=0) die(json_err('cookie_inactive',-1,'Error: Not Yet Active'));//time还在硬直中
 
-	//如果有旧Cookie
-	if(isset($_COOKIE['uid'])){//获取Cookie对应用户数据,如果key不符合,退出
-		$sql = "SELECT * FROM `user` WHERE `uid` = ";
-		$sql.= (string)intval($_COOKIE['uid']) . ";";//防注入
-		$userC= $mysql->getLine($sql);
-		if($mysql->errno()!= 0)
-			die("Error:" . $mysql->errmsg());//SQL出错
-		if($mysql->affectedRows()!=1)
-			die("Error: Cookie Not Exists"); //uid不存在
-		if($userC['key']!=$_COOKIE['key'])
-			die("Error: Invalid Cookie");    //key不符合,!=代表作为数字比较
-		if($userC['status']==0)
-			die("Error: Deleted Cookie");    //status不活跃,==代表作为数字比较
-		if($userC['time']>time())
-			die("Error: Not Yet ");          //time还在硬直中
-	} else die("No Cookie");
-	
-//读取参数: BTIH
-	//检验BTIH有效性并小写化,"magnet:?xt=urn:btih:"长度为20,btih长度为40
-	//即使btih仅由0-9组成也没关系,因为代码中不存在hex与unhex
-	$btih=(string)$_REQUEST['btih'];//字符串
-	if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0)	//如果是完整磁链
-		$btih=substr($btih,20,40);										//截取btih
-	if(strlen($btih)!==40 or !ctype_xdigit($btih))//防注入
-		die("Error: Link Not Valid.");
-	$btih= strtolower($btih);
+//$_GET和$_REQUEST已经urldecode()了！
+//读取参数btih,并字符串化,小写化
+$btih=trim(strtolower(strval($_REQUEST['btih'])));//读取参数btih
+//如果是完整磁链,截取btih,btih长度为40
+$pos=strpos($btih,"btih:");//len('btih:')===5
+$btih=($pos===FALSE)?substr($btih,$pos+5,40):substr($btih,0,40);//注意$pos会自动转换,而$pos=0和$pos=FALSE截取时有区别
+//检验btih长度(应该<=40)与有效性,即使btih仅由0-9组成也没关系,参见http://www.cnblogs.com/mincyw/archive/2011/02/10/1950733.html
+if(strlen($btih)!==40 or !ctype_xdigit($btih)) die(json_err('btih_incorrect',-1,'Error: Link Not Correct'));
 
-//查询btih是否还不存在
-	//查询视频是否已经存在,如BTIH已存在,退出
-	$sql = "SELECT * FROM `video` WHERE `btih` = x'" . $btih . "';";
-	$check = $mysql->getLine($sql);
-	if($mysql->errno()!= 0)
-		die("Error:" . $mysql->errmsg());	//出错
-	if($mysql->affectedRows()!=0)			
-		die("Error: Video Already Exists"); //非空
+//查询视频是否已经存在,如btih不存在,退出
+$result=NULL;//d_index出错不会有严重影响,只要更新就好
+$count=safe_query("SELECT `c_index` `dislike` `d_index` FROM `video` WHERE `btih` = UNHEX(?);",
+		&$result, array('s',$btih));//http://stackoverflow.com/questions/1747894/
+if($count!=0) die(json_err('btih_created',-1,'Error: Video Already Exists'));//返回空
 
-//MySQL添加btih
-	$sql = "INSERT INTO `video` (uid`, `time`, `view`, `reply`, `btih`) VALUES ('";
-	$sql.= $userC['uid'] . ", " .time() . ", 0, 0, x'" . $btih . "';";
-	$mysql->runSql( $sql );
-	if($mysql->errno() != 0) 
-		die("Error:" . $mysql->errmsg());	//出错
+//添加btih
+$blackhole=NULL;
+$count=safe_query("INSERT INTO `video` (uid`, `time`, `view`, `reply`, `btih`,
+		`comment`, `c_index`, `linkage`, `l_index`, `dislike`, `d_index`) 
+		VALUES (?, ?, 0, 0, ?, '', '[]', '[]', '[]', '[]', '[]');",
+		&$blackhole, array('iis', $uid, time(), $bith));//主键自增,comment赋空字符串,其余元素赋空数组
+if($count!=1){
+	$error_info=json_err('video_notcreated',-1,'Error: Failed to Create New Video');
+	die($error_info);//返回空
+}
 
-//KVDB添加初始元素: 弹幕池,链接池,举报池
-
-	//Comment
-	if(!$kv->set($btih . ",c", ""))//string
-		die("Error:" . $kv->errno());
-	if(!$kv->set($btih . ",ci", array()))//json
-		die("Error:" . $kv->errno());
-	//Link
-	if(!$kv->set($btih . ",l",  array()))//array
-		die("Error:" . $kv->errno());
-	if(!$kv->set($btih . ",li", json_encode(array())))//json
-		die("Error:" . $kv->errno());
-	//Dislike	
-	if(!$kv->set($btih . ",d",  array()))//array
-		die("Error:" . $kv->errno());
-	if(!$kv->set($btih . ",di", json_encode(array())))//json
-		die("Error:" . $kv->errno());
-
-//提高积分并暂时硬直
-	$userC['score'] = (int)$userC['score']+$const_ScoreNewVideo;
-	$userC['time']  = time() +$const_DelayNewVideo;
-	$sql = "UPDATE `user` SET `score` = " . (int)$userC['score'];
-	$sql.= ", `time` = " . $userC['time'];
-	$sql.= " WHERE `uid` = " . $_COOKIE['uid'] . ";";
-	$mysql->runSql( $sql );
-	if($mysql->errno() != 0) 
-		die("Error:" . $mysql->errmsg());	//出错
-
+//提高积分并暂时硬直[uid,key,time,point,status]
+$blackhole=NULL;
+$count=safe_query("UPDATE `user` SET `score` = `score` + ?, `time` = `time` + ? WHERE `uid` = ?;", &$blackhole, 
+		array('iii', $const_ScoreNewComment, $const_DelayNewComment, $uid));
 //返回成功页面
-	echo "Video Created Successfully!";
-
-// 关闭数据库
-	$mysql->closeDb();
-
-//关闭kvdb无语句
-	exit;
+exit(json_err('newComment',0,"Video Created Successfully!"));
 ?>

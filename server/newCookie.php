e4785cbca3eb5677aa43b0a2656d362997f9f2bb
Schezuk <schezuk@163.com>
2015-01-04 03:47:26 +0800

    with Lib

--- server/newCookie.php
+++ server/newCookie.php
@@ -1,6 +1,6 @@
 <?php
-//必须先导入startup.sql
-require('libMysqli.php');
+require 'libMysqli.php';
+require 'libFunction.php';
 header("Access-Control-Allow-Origin: *");//无限制
 
 //$_GET和$_REQUEST已经urldecode()了！
@@ -13,55 +13,33 @@ if($_SESSION['vcode'] != $_REQUEST['vcode']) {
 }
 
 //如果有旧Cookie
-if(isset($_COOKIE['uid'])){
-//获取Cookie对应用户数据,如果key不符合,退出
-	$result=NULL;
-	$count=safe_query('SELECT * FROM `user` WHERE `uid` = ?;', &$result, array('i',intval($_COOKIE['uid'])));
-	if($count!=1){
-		$error_info=json_err('cookie_invalid',-1,'Error: Invalid Cookie');
-		die($error_info);//返回空
-	}
-	if($result[0]['key']!=$_COOKIE['key']){
-		$error_info=json_err('cookie_wrongkey',-1,'Error: Cookie with Wrong Key');
-		die($error_info);//key不符合,!=代表作为数字比较
-	}
-	if($result[0]['status']==0){
-		$error_info=json_err('cookie_deleted',-1,'Error: Deleted Cookie');
-		die($error_info);//status禁用,==代表作为数字比较
-	}
-
-//以上通过,则取最新用户数据,如果uid重复,退出
+if(isset($_COOKIE['uid'])) {
+	//获取Cookie对应用户数据,如果key不符合,退出
+	checkCookie();
+	$uid=getUid();
+	//以上通过,则取最新用户数据,如果uid重复,退出
 	$result=NULL;
 	$count=safe_query('SELECT * FROM `user` ORDER BY `uid` DESC LIMIT 1;', &$result);
 	//SELECT * FROM `USER` WHERE `uid` IN (SELECT max(id) FROM `USER`);
-	if($count!=1){
-		$error_info=json_err('user_notexist',-1,'Error: No Users in Database at All');//必须先导入startup.sql
-		die($error_info);//返回空
-	}
-	if(($result[0]['uid']==$_COOKIE['uid']){
-		$error_info=json_err('cookie_lastuser',-1,'Error: You Already Have the Latest Cookie');
-		die($error_info);//已经最新
-	}
-
-//以上通过,则关闭当前Cookie对应用户数据
+	if($count!=1) 
+		die(json_err('user_notexist',-1,'Error: No Users in Database at All'));//必须先导入startup.sql
+	if(($result[0]['uid']===$uid) 
+		die(json_err('cookie_lastuser',-1,'Error: You Already Have the Latest Cookie'));//已经最新
+	//以上通过,则关闭当前Cookie对应用户数据
 	$blackhole=NULL;
 	$count=safe_query('UPDATE `user` SET `status` = 0 WHERE `uid` = ?;', &$blackhole, array('i',intval($_COOKIE['uid'])));
-	if($count!=1){
-		$error_info=json_err('user_notclosed',-1,'Error: Failed to Close Cookie');
-		die($error_info);//返回空
-	}
+	if($count!=1)
+		die(json_err('user_notclosed',-1,'Error: Failed to Close Cookie'));//返回空
 }else{//不论有没有Cookie都要获取最近Cookie数据
 	$result=NULL;
 	$count=safe_query('SELECT * FROM `user` ORDER BY `uid` DESC LIMIT 1;', &$result);
 	//SELECT * FROM `USER` WHERE `uid` IN (SELECT max(id) FROM `USER`);
-	if($count!=1){
-		$error_info=json_err('user_notexist',-1,'Error: No Users in Database at All');//必须先导入startup.sql
-		die($error_info);//返回空
-	}
+	if($count!=1)
+		die(json_err('user_notexist',-1,'Error: No Users in Database at All'));//必须先导入startup.sql
 }
 
 //然后获取下一个Cookie
-$uid   = $result[0]['uid']+1;
+$uid   = $result[0]['uid']+1;//无论如何都要取最近user的原因,因为封装我不能访问last_affected_id
 $key   = rand(0, 4294967295);
 $time  = time()+ 0;//观察期,暂定为新Cookie立刻可以发言
 $point = 100; 
@@ -70,15 +48,11 @@ $status= 1;
 //保存新账号到数据库
 $blackhole=NULL;
 $count=safe_query('INSERT INTO `user` VALUES (?, ?, ?, ?, ?);', &$blackhole, array('iiiii',$uid,$key,$time,$point,$status);
-if($count!=1){
-	$error_info=json_err('user_notcreated',-1,'Error: Failed to Create New Cookie');
-	die($error_info);//返回空
-}
+if($count!=1)
+	die(json_err('user_notcreated',-1,'Error: Failed to Create New Cookie')_;//返回空
 
 //设置Cookie
 setcookie("uid", $uid, 2147483647);//Cookie永不过期
 setcookie("key", $key, 2147483647);//Cookie永不过期
-echo "New Cookie Begotten!";
-exit;
-// 用不着关闭MySQL
+exit(json_err('cookie_create',0,'New Cookie Begotten!'));// 用不着关闭MySQL
 ?>

7e6e02eb46af2660876bc0c8d3c8237fd6c24598
Schezuk <schezuk@163.com>
2015-01-04 03:16:48 +0800

    with Lib

--- server/getDislike.php
+++ server/getDislike.php
@@ -1,18 +1,13 @@
 <?php
 require('libMysqli.php');
+require('libFunction.php');
 header("Access-Control-Allow-Origin: *");//无限制
 	
 //读取参数btih,并字符串化,小写化
-$btih=trim(strtolower(strval($_REQUEST['btih'])));//读取参数btih
-//如果是完整磁链,截取btih,btih长度为40
-$pos=strpos($btih,"btih:");//len('btih:')===5
-$btih=($pos===FALSE)?substr($btih,$pos+5,40):substr($btih,0,40);//注意$pos会自动转换,而$pos=0和$pos=FALSE截取时有区别
-//检验btih长度(应该<=40)与有效性,即使btih仅由0-9组成也没关系,参见http://www.cnblogs.com/mincyw/archive/2011/02/10/1950733.html
-if(strlen($btih)!==40 or !ctype_xdigit($btih)) die(json_err('btih_incorrect',-1,'Error: Link Not Correct'));
-
+$btih=getBtih();
 $result=NULL;
-$count=safe_query("SELECT `d_index` FROM `video` WHERE `btih` = UNHEX(?);",
-		&$result, array('s',$btih));//http://stackoverflow.com/questions/1747894/
-if($count!=1) die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//返回空
-exit(return[0]['d_index']);
+$count=safe_query("SELECT `d_index` FROM `video` WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
+if($count!=1) 
+	die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//无返回值
+exit(return[0]['d_index']);//返回字段d_index,该字段是json字符串
 ?>

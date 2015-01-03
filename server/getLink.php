b26f3cd7ceb2b4af76049930c7c7ada1bc5486da
Schezuk <schezuk@163.com>
2015-01-03 09:27:02 +0800

    test

--- server/getLink.php
+++ server/getLink.php
@@ -1,20 +1,18 @@
-﻿<?php
-	header("Access-Control-Allow-Origin: *");
-	
-	//打开KVDB
-	$kv = new SaeKV();
-	if(!$kv->init()) die("Error:" . $kv->errno());//出错
-	
-	//检验BTIH有效性并小写化,"magnet:?xt=urn:btih:"长度为20,btih长度为40
-	$btih=(string)$_REQUEST['btih'];//字符串
-	if(strlen($btih)>=60 and strpos($btih,"magnet:?xt=urn:btih:")===0)
-		$btih=substr($btih,20,40);
-	if(strlen($btih)!==40 or !ctype_xdigit($btih))//防注入
-		die("Error: Link Not Valid.");
-	$btih= strtolower($btih);
-	//即使btih仅由0-9组成也没关系,因为代码中不存在hex与unhex
+<?php
+require('libMysqli.php');
+header("Access-Control-Allow-Origin: *");//无限制
 
-	if($ret = $kv->get($btih . ",li")) echo $ret;//赋值运算表达式的值也就是所赋的值
-	else die("Error:" . $kv->errno());
-	
+//读取参数btih,并字符串化,小写化
+$btih=trim(strtolower(strval($_REQUEST['btih'])));//读取参数btih
+//如果是完整磁链,截取btih,btih长度为40
+$pos=strpos($btih,"btih:");//len('btih:')===5
+$btih=($pos===FALSE)?substr($btih,$pos+5,40):substr($btih,0,40);//注意$pos会自动转换,而$pos=0和$pos=FALSE截取时有区别
+//检验btih长度(应该<=40)与有效性,即使btih仅由0-9组成也没关系,参见http://www.cnblogs.com/mincyw/archive/2011/02/10/1950733.html
+if(strlen($btih)!==40 or !ctype_xdigit($btih)) die(json_err('btih_incorrect',-1,'Error: Link Not Correct'));
+
+$result=NULL;
+$count=safe_query("SELECT `l_index` FROM `video` WHERE `btih` = UNHEX(?);",
+		&$result, array('s',$btih));//http://stackoverflow.com/questions/1747894/
+if($count!=1) die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//返回空
+exit(return[0]['l_index']);
 ?>

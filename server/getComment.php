b10bb009e5940f9af7ff8f073917ccd313a0910f
Schezuk <schezuk@163.com>
2015-01-07 12:57:18 +0800

    Update getComment.php

--- server/getComment.php
+++ server/getComment.php
@@ -18,10 +18,13 @@ case "cid":{//********按弹幕号[start,end]获取,计入view,参数为start,en
 	//计入view
 	$blackhole=NULL;
 	$count=safe_query("UPDATE `video` SET `view` =`view` + 1 WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
+	//如果弹幕数少于1
+	$c_count=count($c_index);//int count()不返回boolean
+	if($c_count<0)) exit '[]';//是否计入view应取决于action的参数,因此代码应放在case里
 
 	//有效化start/end参数,cid范围[0,count-1]
 	$cid_start = 0;	//设定起终点默认值
-	$cid_end   = count($c_index)-1;
+	$cid_end   = $c_count - 1;
 	//如果参数存在且有效,更新起终点
 	if(isset($_REQUEST['start'])){
 		$temp_start= intval(trim($_REQUEST['start']));
@@ -34,14 +37,18 @@ case "cid":{//********按弹幕号[start,end]获取,计入view,参数为start,en
 	if($cid_end < $cid_start) {$temp=$cid_start; $cid_start=$cid_end; $cid_end=$temp;}
 	//定位并输出弹幕,function substr(字符串,截取掉的开头字符数,输出的剩余字符数)
 	$str_start = ($cid_start == 0)? 0 :($c_index[$cid_start-1][2]);	//省去的开头部分的长度,即剩余串第一个字符的下标
-	$str_end   = $c_index[$cid_end][2]				//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
+	$str_end   = $c_index[$cid_end][2];				//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
 	exit ("[" . substr($comment,$str_start,$str_end-$str_start-1) . "]");//结尾逗号要去掉,echo接受多参数,且不会补充空格
+	//因为$cid_start<=$cid_end,所以至少输出1项;又因为项长不为零,所以$str_end-$str_start-1不为负
 	}
 break;//其实无用
 case "time":{//********按时间[start,end]来获取,计入view,参数为start,end
 	//计入view
 	$blackhole=NULL;
 	$count=safe_query("UPDATE `video` SET `view` =`view` + 1 WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
+	//如果弹幕数少于1
+	$c_count=count($c_index);//int count()不返回boolean
+	if($c_count<0)) exit '[]';//是否计入view应取决于action的参数,因此代码应放在case里
 
 	//有效化start/end参数,time范围[0,now]
 	$time_start = 0;	//设定起终点默认值
@@ -58,19 +65,16 @@ case "time":{//********按时间[start,end]来获取,计入view,参数为start,e
 	if($time_end < $time_start) {$temp=$time_start; $time_start=$time_end; $time_end=$temp;}
 	//获取定位,借鉴STL算法http://www.it165.net/pro/html/201404/11813.html,参见stackoverflow.com/questions/27322112
 	//查找右边界
-	$len  = count($c_index);//在[0,count-1]中搜索
-	$last = $len - 1;
+	$len  = $c_count;//在[0,count-1]中搜索
+	$last = $c_count - 1;
 	//int $middle, $half;
 	while($len > 0) {
 		$half   = $len >> 1;
 		$middle	= $last - $half;//此时$last,$half初始化完成
-		if($c_index[middle][1] > $time_end) {$last = $middle - 1; $len = $len - $half - 1;}//在左边子序列中查找
+		if($c_index[$middle][1] > $time_end) {$last = $middle - 1; $len = $len - $half - 1;}//在左边子序列中查找
 		else $len = $half;//在右边子序列（包含middle）中查找
 	}
 	//return $last;	//此时len=0,last=[-1,count-1],last为右边界元素的下标
-	//当last=-1时,即所有弹幕发布时间都晚于$time_end,那么返回空值(而非报错)
-	exit("[]");
-
 	//查找左边界
 	$len = $last + 1;//在[0,last]中搜索
 	$first = 0;
@@ -78,20 +82,30 @@ case "time":{//********按时间[start,end]来获取,计入view,参数为start,e
 	while($len > 0) {
 		$half   = $len >> 1;
 		$middle	= $first + $half;//此时$last,$half二次初始化完成
-		if($c_index[middle][1] < > $time_start) {$first = $middle + 1; $len = $len - $half - 1;}//在右边子序列中查找
+		if($c_index[$middle][1] < > $time_start) {$first = $middle + 1; $len = $len - $half - 1;}//在右边子序列中查找
 		else $len = $half;//在左边子序列（包含middle）中查找
 	}
 	//return $first; //此时len=0，first=[0,last],但是(而且只有)当last=count-1的时候first可能等于last+1,即count
-	//当first=count=last+1时,即所有弹幕发布时间都早于$time_start,那么返回空值(而非报错)
-	exit("[]");
-	
+
+	//返回空值的情况(其他case均不涉及)
+	//当$time_end  早于所有弹幕发布时间,即last=-1时: len再次被赋为0,第二个while不执行,first=0
+	//当$time_start晚于所有弹幕发布时间,即last=count-1,而first=count
+	//当[$time_start,$time_end]居于相邻两条弹幕之间,即last=###,而first=### + 1
+	//以上情况都是first=last + 1,(注意first=last时存在一条弹幕)
+	if ($last<$first) exit("[]");//那么返回空值(而非报错)
+
 	//定位并输出弹幕,function substr(字符串,截取掉的开头字符数,输出的剩余字符数)
 	$str_start = ($first == 0)? 0 :($c_index[$first-1][2]);	//省去的开头部分的长度,即剩余串第一个字符的下标
-	$str_end   = $c_index[$last][2]				//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
+	$str_end   = $c_index[$last][2];			//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
 	exit ("[" . substr($comment,$str_start,$str_end-$str_start-1) . "]");//结尾逗号要去掉,echo接受多参数,且不会补充空格
+	//因为first<=last,所以至少输出1项;又因为项长不为零,所以$str_end-$str_start-1不为负
 	}
 break;//其实无用
 case "recent":{//********获取下一条到最后一条,不计入view,参数为start
+	//如果弹幕数少于1
+	$c_count=count($c_index);//int count()不返回boolean
+	if($c_count<0)) exit '[]';//是否计入view应取决于action的参数,因此代码应放在case里
+
 	//有效化start/end参数,cid范围[0,count-1]
 	$cid_start = 0;	//设定起终点默认值
 	$cid_end   = count($c_index)-1;
@@ -102,16 +116,25 @@ case "recent":{//********获取下一条到最后一条,不计入view,参数为s
 	} 
 	//定位并输出弹幕,function substr(字符串,截取掉的开头字符数,输出的剩余字符数)
 	$str_start = ($cid_start == 0)? 0 :($c_index[$cid_start-1][2]);	//省去的开头部分的长度,即剩余串第一个字符的下标
-	$str_end   = $c_index[$cid_end][2]				//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
+	$str_end   = $c_index[$cid_end][2];				//剩余串最后字符的下标,差值是剩余串长(含结尾逗号)
 	exit ("[" . substr($comment,$str_start,$str_end-$str_start-1) . "]");//结尾逗号要去掉,echo接受多参数,且不会补充空格
+	//因为$cid_start<=$cid_end,所以至少输出1项;又因为项长不为零,所以$str_end-$str_start-1不为负
 	}
 break;//其实无用
 case "last":{//********获取最后count条,计入view,参数为count
+	//计入view
+	$blackhole=NULL;
+	$count=safe_query("UPDATE `video` SET `view` =`view` + 1 WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
+	//如果弹幕数少于1
+	$c_count=count($c_index);//int count()不返回boolean
+	if($c_count<0)) exit '[]';//是否计入view应取决于action的参数,因此代码应放在case里
+
 	//有效化start/end参数,cid范围[0,count-1]
 	$cid_start = 0;	//设定起终点默认值
 	$cid_end   = count($c_index)-1;
 	if(isset($_REQUEST['count'])){
 		$c_count = intval(trim($_REQUEST['count']));
+		$c_count 
 		$cid_start = ($c_count > $cid_end)? 0 :$cid_end + 1 - $c_count;//
 		//比如有012三条,c_count=1则cid_start=2;c_count=2则cid_start=1;c_count=3则cid_start=0;c_count=4则cid_start=0
 		$temp_start= intval(trim($_REQUEST['start']));

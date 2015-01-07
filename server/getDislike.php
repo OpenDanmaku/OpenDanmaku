30ecde923939e669fbb69f1213ef3dba28007bbe
Schezuk <schezuk@163.com>
2015-01-07 21:35:50 +0800

    with Lib
    correcting errors

--- server/getDislike.php
+++ server/getDislike.php
@@ -9,5 +9,5 @@ $result=NULL;
 $count=safe_query("SELECT `d_index` FROM `video` WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
 if($count!=1) 
 	die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//无返回值
-exit(return[0]['d_index']);//返回字段d_index,该字段是json字符串
+exit($result[0]['d_index']);//返回字段d_index,该字段是json字符串
 ?>

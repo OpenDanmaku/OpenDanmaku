57ec162e8d7a27b06b1f3cdf934af32a89a62202
Schezuk <schezuk@163.com>
2015-01-07 10:19:26 +0800

    Update getComment.php

--- server/getComment.php
+++ server/getComment.php
@@ -5,6 +5,7 @@ header("Access-Control-Allow-Origin: *");//无限制
 	
 //读取参数btih,并字符串化,小写化
 $btih=getBtih();
+//获取comment与c_index字段
 $result=NULL;
 $count=safe_query("SELECT `comment`, `c_index` FROM `video` WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
 if($count!=1) die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));
@@ -13,20 +14,35 @@ $comment=$result[0]['comment'];
 
 //获取任务
 if(isset($_REQUEST['action'])){
-	switch (strtolower($_REQUEST['action'])){
-	    case "cid":			//********按弹幕号[start,end]获取,计入view,参数为start,end
-	    	{
-			//获取cid起终点参数,cid范围[0,count-1]
-				//获取起点
-				$cid_start = 0;												//假设起点为0
-				if(isset($_REQUEST['start'])) $temp=(int)$_REQUEST['start'];	//如果提交起点
+	switch (strtolower(trim($_REQUEST['action']))){
+	case "cid":{//********按弹幕号[start,end]获取,计入view,参数为start,end
+		//有效化start/end参数,cid范围[0,count-1]
+		//设定起终点默认值
+		$cid_start = 0;
+		$cid_end   = count($c_index)-1;
+		//如果参数存在且有效,更新起终点
+		if(isset($_REQUEST['start'])){
+			$temp_start= intval(trim($_REQUEST['start']));
+			if (0 <= $temp_start and $temp_start <= $cid_end) $cid_start = $temp_start;
+		} 
+		if(isset($_REQUEST['end'])){
+			$temp_end  = intval(trim($_REQUEST['end']));
+			if (0 <= $temp_end   and $temp_end   <= $cid_end) $cid_end   = $temp_end  ;//赋值前$cid_end仍等于上限
+		} 	
+		if($cid_end < $cid_start) {$temp=$cid_start; $cid_start=$cid_end; $cid_end=$temp;}
+		$temp_end  = intval(trim($_REQUEST['end']));
+		
+			$cid_start = ?
+		
+		$temp=i;//如果提交起点则更新
+		
 				if($temp >= 0 and $temp <= $count-1) $cid_start = $temp;		//且居于[0,count-1]则重设起点
 				//获取终点
 				$cid_end = $count-1;										//假设终点为$count-1
 				if(isset($_REQUEST['end']))   $temp=(int)$_REQUEST['end'];  	//如果提交终点
 				if($temp >= 0 and $temp <= $count-1) $cid_end   = $temp;		//且居于[0,count-1]则重设起点
 				//如果start/end错位交换顺序,当然start/end任意一个为空不会有此问题
-				if($cid_end < $cid_start) {$temp=$cid_start;$cid_start=$cid_end;$cid_end=$temp;}
+				
 				//以上三个步骤$temp因为先被赋值所以不会串味
 			//定位并输出弹幕
 				$str_start = ($cid_start == 0) ? 0 : ($c_index[$cid_start-1][2]);//省去的开头部分的长度,即剩余串第一个字符的下标
@@ -54,7 +70,42 @@ if(isset($_REQUEST['action'])){
 				//如果start/end错位交换顺序,当然start/end任意一个为空不会有此问题
 				if($time_end < $time_start) {$temp=$time_start;$time_start=$time_end;$time_end=$temp;}
 				//以上三个步骤$temp因为先被赋值所以不会串味
-		
+		//查找右边界
+int last = size - 1;
+int len  = size;
+int middle, half, len;
+	len = size;
+
+	while(len > 0) {
+		half	= len >> 1;
+		middle	= last - half;
+		if(array[middle] > key) {	 
+			last	= middle - 1;		  
+			len	= len - half - 1;	//在左边子序列中查找
+		}
+		else
+			len	= half;			//在右边子序列（包含middle）中查找
+	}
+	return last;
+
+	//查找左边界
+	int first = 0;
+	int middle, half, len;
+	len = last + 1;
+
+	while(len > 0) {
+		half	= len >> 1;
+		middle	= first + half;
+		if(array[middle] < key) {	 
+			first	= middle + 1;		  
+			len	= len - half - 1;	//在右边子序列中查找
+		}
+		else
+			len	= half;			//在左边子序列（包含middle）中查找
+	}
+	return first;
+}
+
 			//获取定位,借鉴STL算法http://www.cnblogs.com/cobbliu/archive/2012/05/21/2512249.html
 			//首先从[0,count-1]获得终点(小于等于某时间的第一个),-1表示不存在
 				$cid_start = $count - 1;

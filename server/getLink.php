f8cf4310e4859917235e17c790956f4f35299834
Schezuk <schezuk@163.com>
2015-01-07 21:36:39 +0800

    with Lib

--- server/getLink.php
+++ server/getLink.php
@@ -9,5 +9,5 @@ $result=NULL;
 $count=safe_query("SELECT `l_index` FROM `video` WHERE `btih` = UNHEX(?);",&$result, array('s',$btih));
 if($count!=1) 
 	die(json_err('btih_unavailable',-1,'Error: Video Not Yet Exists, Do You Want to Create It?'));//无返回值
-exit(return[0]['l_index']);//返回字段l_index,该字段是json字符串
+exit($result[0]['l_index']);//返回字段l_index,该字段是json字符串
 ?>
